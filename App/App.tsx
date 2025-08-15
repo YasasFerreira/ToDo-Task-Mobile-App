13bebf8729c78b8918c4c1318bf2377b8c4598d7
Yasas Ferreira <139609496+YasasFerreira@users.noreply.github.com>
2025-08-15 06:11:20 +0000

    updated App.tsx

--- App/App.tsx
+++ App/App.tsx
@@ -1,12 +1,68 @@
+import React from "react"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./screens/HomeScreen"
+import TaskScreen from "./screens/TaskScreen"
+import EditTask from "./screens/EditTask"
+import WelcomeScreen from "./screens/WelcomeScreen";
+import {COLORS} from "./constants/theme"
+
+export type RootStackParamList = {
+  Welcome:undefined;
+  Home: undefined;
+  Task: undefined;
+  EditTask: { task: any }; // must match what you pass from HomeScreen
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator
+        initialRouteName={"Welcome"}
+          screenOptions={{
+            headerShown: true, // Enable headers by default
+            headerStyle: {
+              backgroundColor: COLORS.background // Match StatusBar color
+            },
+            headerTintColor: '#000', // Text color for header
+            headerTitleStyle: {
+            fontWeight: 'bold',
+            },
+        }}
+      >
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+          
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          
+        />
+        <Stack.Screen
+          name="Task" 
+          component={TaskScreen}
+        />
+
+        <Stack.Screen 
+          name="EditTask" 
+          component={EditTask} 
+        />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
